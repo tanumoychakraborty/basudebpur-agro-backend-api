1a7b48b785ddcf16e2aeec43a40f7fe7545af72d
tanumoy chakraborty <tanumoy.chakraborty.2010@gmail.com>
2019-04-11 02:17:48 +0530

    Update SerializerMiddleware.py

--- util/SerializerMiddleware.py
+++ util/SerializerMiddleware.py
@@ -20,12 +20,12 @@ class SerializerMiddleware(object):
             '''
             for post man
             '''
-            #req_data = json.loads(req.stream.read().decode("utf-8"))
+            req_data = json.loads(req.stream.read().decode("utf-8"))
             '''
             for django
             '''
-            print('incoming >>>>>>>'+str(req.media))
-            req_data = req.media
+            #print('incoming >>>>>>>'+str(req.media))
+            #req_data = req.media
             
         try:
             serializer = resource.serializers[req.method.lower()]
