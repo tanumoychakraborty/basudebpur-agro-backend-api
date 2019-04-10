b5ee0a66623c66588a5823d85e40b6a6faab0150
tanumoy chakraborty <tanumoy.chakraborty.2010@gmail.com>
2019-04-11 03:53:41 +0530

    Update SerializerMiddleware.py

--- util/SerializerMiddleware.py
+++ util/SerializerMiddleware.py
@@ -37,5 +37,5 @@ class SerializerMiddleware(object):
                     data=req_data
                 )
             except ValidationError as err:
-                print(str('validation error >>>>>>>>>>'+err.messages))
+                print(str('validation error >>>>>>>>>>'+str(err.messages))
                 raise HTTPError(status=status_codes.HTTP_422, errors=err.messages)
